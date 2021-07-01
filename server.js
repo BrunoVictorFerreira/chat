baa6a475530037f563e2be699fcc1e6451213dc0
BrunoVictorFerreira <victorbruno221@gmail.com>
2021-07-01 10:17:05 -0300

    fixing a error not responding

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 
 const app = express();
-const server = require("http").createServer(app);
+const server = require("http").createServer(app).listen(3000, '127.0.0.1', () => console.log('servidor rodando'));
 const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname, "public")));
